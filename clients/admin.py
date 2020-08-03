248613643b4a2ff8d004db210d8fe246ce1240a6
Lucas Silveira <lugano1995@gmail.com>
2020-08-03 01:02:20 -0300

    feat: create sales and products model

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Document, Sale, Product
 
 admin.site.register(Client)
+admin.site.register(Document)
+admin.site.register(Sale)
+admin.site.register(Product)
--- clients/models.py
+++ clients/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Document(models.Model):
+    number = models.CharField(max_length=20)
+    issuing_state = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.number
+
+
 class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -9,6 +17,27 @@ class Client(models.Model):
     bio = models.TextField()
     avatar = models.ImageField(
         upload_to='clients_avatar', null=True, blank=True)
+    doc = models.OneToOneField(Document, null=True, blank=True,
+                               on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Sale(models.Model):
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    client = models.ForeignKey(
+        Client, null=True, blank=True, on_delete=models.SET_NULL)
+    products = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self):
+        return self.pk
